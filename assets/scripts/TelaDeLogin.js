3d47c8df1b472c0256b77d9b0d488bf53e62dfba
GABRIEL OLSON WAECHTER <4553287268@cedup.local>
2025-10-02 15:29:20 -0300

    colocando script login

--- assets/scripts/TelaDeLogin.js
+++ assets/scripts/TelaDeLogin.js
@@ -9,5 +9,27 @@ document.addEventListener("DOMContentLoaded", function () {
     };
 
     //TODO fazer o fetch
+    login(dados.email, dados.senha)
   };
 });
+
+async function login(email, senha) {
+  const resposta = await fetch("/api/login",{
+    method:"post",
+    body:JSON.stringify({
+      email,
+      senha
+    }),headers:{
+      "Content-Type": "application/json"
+    }
+  })
+  const json = await resposta.json()
+  if(json.erro){
+    alert(json.mensagem)
+    return
+  }else{
+    const token = json.token
+    localStorage.setItem("token", token)
+    window.location.assign("/suporte")
+  }
+}
