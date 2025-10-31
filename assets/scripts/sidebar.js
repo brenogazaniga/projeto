912306fee3587ec0df5d7e309489427a92f09414
JOAO EDUARDO ANDRETTO <4553581115@cedup.local>
2025-10-31 17:13:13 -0300

    recolhimento da sidebar

--- assets/scripts/sidebar.js
+++ assets/scripts/sidebar.js
@@ -1,9 +1,14 @@
-
-document.addEventListener("DOMContentLoaded", () => {
-    const aside = document.querySelector("aside")
-    const hamburguer = document.querySelector(".hamburguer")
-
-    hamburguer.addEventListener("click", () => {
-        aside.classList.toggle("show")
-    })
-})
+document.addEventListener('DOMContentLoaded', function() {
+    const logoToggle = document.getElementById('logo-toggle');
+    const sidebar = document.getElementById('barra-lateral');
+    logoToggle.addEventListener('click', function() {
+        if (window.innerWidth <= 2000) {
+            sidebar.classList.toggle('show'); 
+        }
+    });
+    document.addEventListener('click', function(event) {
+        if (!sidebar.contains(event.target) && !logoToggle.contains(event.target) && window.innerWidth <= 992) {
+            sidebar.classList.remove('show');
+        }
+    });
+});
