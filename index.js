aae1be256df783e8ab5eae9ebaaad351103e8378
JoaoEduardoAndretto <jjoaoaandretto@gmail.com>
2025-11-03 15:46:38 -0300

    fix location index js file

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ const path = require("path");
 const cors = require("cors");
 const cookieparser = require("cookie-parser");
 
-const { rotaUsuario } = require("./controllers/usuario");
-const { rotaMetrica } = require("./controllers/metricas");
-const { rotaPaginas } = require("./controllers/paginas");
-const { rotaLogin } = require("./controllers/login");
-const { rotaChatia } = require("./controllers/chatia");
+const { rotaUsuario } = require("./src/controllers/usuario");
+const { rotaMetrica } = require("./src/controllers/metricas");
+const { rotaPaginas } = require("./src/controllers/paginas");
+const { rotaLogin } = require("./src/controllers/login");
+const { rotaChatia } = require("./src/controllers/chatia");
 
-const { db } = require("./db");
+const { db } = require("./src/db");
 
 const server = express();
 server.use(express.static(path.join(__dirname, "assets")));
@@ -17,15 +17,15 @@ server.use("/assets", express.static("assets"));
 server.use(cors());
 server.use(cookieparser());
 server.use(express.json());
-server.use(express.static(path.join(__dirname, "../pages/TelaCadastro")));
-server.use(express.static(path.join(__dirname, "../pages/TelaDeLogin")));
+server.use(express.static(path.join(__dirname, "./pages/TelaCadastro")));
+server.use(express.static(path.join(__dirname, "./pages/TelaDeLogin")));
 server.use(rotaUsuario);
 server.use(rotaMetrica);
 server.use(rotaPaginas);
 server.use(rotaLogin);
 server.use(rotaChatia);
 server.get("/teste", (req, res) => {
-    res.sendFile(path.join(__dirname, "../pages/oauth.html"));
+    res.sendFile(path.join(__dirname, "./pages/oauth.html"));
     
 });
 
