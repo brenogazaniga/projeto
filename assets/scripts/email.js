2e623411b2f7929352f39f471112361df0853f2d
ERYCK JOÃO PEREIRA <4553220287@cedup.local>
2025-11-03 14:37:38 -0300

    teste

---
+++ assets/scripts/email.js
@@ -0,0 +1,13 @@
+
+/*const emailEl = document.getElementById('email');
+
+function handleEmailClick() {
+  const to = 'suporte@exemplo.com';
+  const subject = 'Assunto: Suporte';
+  const body = 'Olá,\n\nDescreva aqui o seu problema.\n\nObrigado!';
+  
+  const mailto = `mailto:${encodeURIComponent(to)}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
+  window.location.href = mailto; // abre o cliente de e-mail padrão do usuário
+}
+
+emailEl.addEventListener('click', handleEmailClick);/*
