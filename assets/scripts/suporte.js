200ad5c35f65f418f794220ebc09880fa9fbebe9
eryckjoao84 <4553220287@cedup.local>
2025-11-06 14:59:55 -0300

    suporte ajustado

--- assets/scripts/suporte.js
+++ assets/scripts/suporte.js
@@ -1,7 +1,5 @@
 document.querySelectorAll(".botao").forEach((el) => {
   el.addEventListener("click", () => {
     el.closest("div").classList.toggle("maior")
-
-    console.log(el.closest("div"))
   })
 })
