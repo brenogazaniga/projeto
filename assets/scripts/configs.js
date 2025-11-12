010f0254fb6823cceb7c29f84ad498eaa12b5a22
Joao Eduardo <125469869+JoaoEduardoAndretto@users.noreply.github.com>
2025-11-12 19:44:53 +0000

    alteração nome, senha e email do usuário

--- assets/scripts/configs.js
+++ assets/scripts/configs.js
@@ -1,23 +1,35 @@
-function showModal(){
-   let modal = document.querySelector(".back-modal")
-   modal.classList.remove("disable")
+function showModal() {
+  let modal = document.querySelector(".back-modal");
+  modal.classList.remove("disable");
 }
 
-function closeModal(){
-  let modal = document.querySelector(".back-modal")
-  modal.classList.add("disable")
+function closeModal() {
+  let modal = document.querySelector(".back-modal");
+  modal.classList.add("disable");
 }
 
-document.querySelector("#logout a").addEventListener("click", async function(event) {
-  event.preventDefault(); 
-  await fetch("/logout")
-  localStorage.removeItem("token"); 
-  window.location.href = "/"; 
+document
+  .querySelector("#logout a")
+  .addEventListener("click", async function (event) {
+    event.preventDefault();
+    await fetch("/logout");
+    localStorage.removeItem("token");
+    window.location.href = "/";
+  });
+
+addEventListener("DOMContentLoaded", async () => {
+  const info = document.querySelector("#infos");
+  const resposta = await fetch("/api/usuario", {
+    headers: {
+      Authentication: "Bearer " + localStorage.getItem("token"),
+    },
+  });
+
+  const dados = await resposta.json();
+  console.log(dados);
+
+  const campos = info.querySelectorAll(".informacoes");
+  campos[0].querySelector("dd").innerHTML = dados.email;
+  campos[1].querySelector("dd").innerHTML = dados.senha;
+  campos[2].querySelector("dd").innerHTML = dados.nome;
 });
-
-addEventListener("DOMContentLoaded", () => {
-  const info = document.querySelector("#infos")
-  info.querySelectorAll(".informacoes").forEach((campo) => {
-    campo.querySelector("dd").innerText = "a"
-  })
-})
